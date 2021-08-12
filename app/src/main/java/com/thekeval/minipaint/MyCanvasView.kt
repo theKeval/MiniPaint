4c0a3381fb9781be4be01e10c1d779bd948a07e9
Keval <keval.langalia@live.com>
2021-08-12 01:49:57 -0400

    upto 4.12 => working on touch events

--- app/src/main/java/com/thekeval/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/thekeval/minipaint/MyCanvasView.kt
@@ -3,8 +3,13 @@ package com.thekeval.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f
 
 class MyCanvasView(context: Context) : View(context) {
 
@@ -12,6 +17,25 @@ class MyCanvasView(context: Context) : View(context) {
     private lateinit var extraBitmap: Bitmap
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+    private var path = Path()
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -28,6 +52,31 @@ class MyCanvasView(context: Context) : View(context) {
         super.onDraw(canvas)
 
         canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+
+    private fun touchStart() {
+
+    }
+
+    private fun touchMove() {
+
+    }
+
+    private fun touchUp() {
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        motionTouchEventX = event!!.x
+        motionTouchEventY = event.y
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+
+        return true
     }
 
 }
